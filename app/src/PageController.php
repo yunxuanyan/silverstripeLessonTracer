bbc2db675a6c5175452dc0aff30a9dcb64839087
Jenny Yan <yunxuan.yan@gmail.com>
2020-09-03 16:17:45 +1200

    Lesson 5 Working with Multiple Templates 1. Add HomePage.php and HomePageController.php,dev/build 2. In CMS, modify the PageType of Home from Page to HomePage in Settings

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -4,6 +4,9 @@ namespace {
 
     use SilverStripe\CMS\Controllers\ContentController;
 
+    use SilverStripe\View\Requirements;
+
+
     class PageController extends ContentController
     {
         /**
@@ -28,6 +31,17 @@ namespace {
             parent::init();
             // You can include any CSS or JS required by your project here.
             // See: https://docs.silverstripe.org/en/developer_guides/templates/requirements/
+            Requirements::css('css/bootstrap.min.css');
+            Requirements::css('css/style.css');
+            Requirements::javascript('javascript/common/modernizr.js');
+            Requirements::javascript('javascript/common/jquery-1.11.1.min.js');
+            Requirements::javascript('javascript/common/bootstrap.min.js');
+            Requirements::javascript('javascript/common/bootstrap-datepicker.js');
+            Requirements::javascript('javascript/common/chosen.min.js');
+            Requirements::javascript('javascript/common/bootstrap-checkbox.js');
+            Requirements::javascript('javascript/common/nice-scroll.js');
+            Requirements::javascript('javascript/common/jquery-browser.js');
+            Requirements::javascript('javascript/scripts.js');
         }
     }
 }
