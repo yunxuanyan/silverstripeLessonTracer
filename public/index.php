75441e849eaf9e73006b706714c997f627594c62
Jenny Yan <yunxuan.yan@gmail.com>
2020-09-03 12:39:01 +1200

    Lesson 3 Migrating static templates into your theme

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use SilverStripe\Control\HTTPApplication;
 use SilverStripe\Control\HTTPRequestBuilder;
 use SilverStripe\Core\CoreKernel;
+use SilverStripe\Core\Startup\ErrorControlChainMiddleware;
 
 // Find autoload.php
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
@@ -21,5 +22,6 @@ $request = HTTPRequestBuilder::createFromEnvironment();
 // Default application
 $kernel = new CoreKernel(BASE_PATH);
 $app = new HTTPApplication($kernel);
+$app->addMiddleware(new ErrorControlChainMiddleware($app));
 $response = $app->handle($request);
 $response->output();
