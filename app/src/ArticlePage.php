278a9c3329ecd2149b6bef233330df2661dd040a
Jenny Yan <yunxuan.yan@gmail.com>
2020-09-04 11:36:46 +1200

    Lesson 7 Working with Files and Images 1.after add photo and brochure fields into the DB, add some photoes and brochure in CMS 2.After add $own in ArticlePage.php, public the ArticlePage in CMS again, then its works

--- app/src/ArticlePage.php
+++ app/src/ArticlePage.php
@@ -8,15 +8,31 @@ use SilverStripe\Forms\DateField;
 use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\TextField;
 
+use SilverStripe\Assets\Image;
+use SilverStripe\Assets\File;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+
 class ArticlePage extends Page
 {
   private static $can_be_root = false;
+
     private static $table_name = 'ArticlePage';
+    
     private static $db = [
         'Date' => 'Date',
         'Teaser' => 'Text',
         'Author' => 'Varchar',
-      ];
+    ];
+
+    private static $has_one = [
+        'Photo' => Image::class,
+        'Brochure' => File::class
+    ];
+
+    private static $owns = [
+      'Photo',
+      'Brochure',
+  ];
     
       public function getCMSFields()
       {
@@ -28,6 +44,14 @@ class ArticlePage extends Page
         );
         $fields->addFieldToTab('Root.Main', TextField::create('Author','Author of article'),'Content');
     
+        $fields->addFieldToTab('Root.Attachments', $photo = UploadField::create('Photo'));
+        $fields->addFieldToTab('Root.Attachments', $brochure = UploadField::create('Brochure','Travel brochure, optional (PDF only)'));
+
+        $photo->setFolderName('travel-photos');
+        $brochure
+          ->setFolderName('travel-brochures')
+          ->getValidator()->setAllowedExtensions(array('pdf'));
+
         return $fields;
       }
 }
